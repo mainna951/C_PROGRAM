7d1701c7c7e7d0d62103f2aa0e60ccc9f0886acc
CT100/G/26131/25 <maina.26131@students.kyu.ac.ke>
2025-10-02 12:07:34 +0300

    Add files via upload

---
+++ MobileData.c
@@ -0,0 +1,44 @@
+//simple C program (Mobile Data)
+/*
+Name: Joseph Thuo
+Reg: CT100/G/26131/25
+Description: C program to prompt user to chose data to subscribe
+*/
+
+#include <stdio.h>
+
+int main() {
+    int choice;
+    
+    // Display the menu
+    printf("Select data bundle:\n");
+    printf("1. 100MB @ 50 KES\n");
+    printf("2. 500MB @ 200 KES\n");
+    printf("3. 1GB @ 350 KES\n");
+    printf("4. 2GB @ 600 KES\n");
+    
+    // Get user input
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+    
+    // Process the choice using switch statement
+    switch(choice) {
+        case 1:
+            printf("You selected 100MB. Cost = 50 KES\n");
+            break;
+        case 2:
+            printf("You selected 500MB. Cost = 200 KES\n");
+            break;
+        case 3:
+            printf("You selected 1GB. Cost = 350 KES\n");
+            break;
+        case 4:
+            printf("You selected 2GB. Cost = 600 KES\n");
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+    
+    return 0;
+}
