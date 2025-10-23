adfbcbd1edd7c63585464bb2fc6815f8f74d1c2a
CT100/G/26131/25 <maina.26131@students.kyu.ac.ke>
2025-10-23 23:58:24 +0300

    Add files via upload

--- caculateElectricBill().c
+++ caculateElectricBill().c
@@ -7,25 +7,29 @@ DESC: Simple c program to calculate electric bill consumed
 
 #include <stdio.h>
 
-// Function to calculate electric bill based on units consumed
 int calculateElectricBill(int units) {
     int bill = 0;
-    
+
     if (units <= 100) {
-        bill = units * 10; // First 100 units at 10 KSh each
+        bill = units * 10;
     } else if (units <= 200) {
-        // First 100 units at 10 KSh + remaining units at 15 KSh
         bill = 100 * 10 + (units - 100) * 15;
     } else {
-        // First 100 units at 10 KSh + next 100 units at 15 KSh + remaining units at 20 KSh
         bill = 100 * 10 + 100 * 15 + (units - 200) * 20;
     }
-    
+
     return bill;
 }
 
 int main() {
-    int units = 250;
-    printf("Total bill for %d units is KSh. %d\n", units, calculateElectricBill(units));
+    int units;
+
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    int totalBill = calculateElectricBill(units);
+    printf("Total bill for %d units is: KSh. %d\n", units, totalBill);
+
     return 0;
 }
+
